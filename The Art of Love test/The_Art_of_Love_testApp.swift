581c54b6d1daa923e9945970ba735706f9c0aa48
Tuhin Mahmud <tuhinm@us.ibm.com>
2023-03-29 21:00:56 -0500

    Initial Commit

---
+++ The Art of Love test/The_Art_of_Love_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_Art_of_Love_testApp.swift
+//  The Art of Love test
+//
+//  Created by Tuhin Mahmud on 3/29/23.
+//
+
+import SwiftUI
+
+@main
+struct The_Art_of_Love_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
